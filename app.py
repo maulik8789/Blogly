ad2e20ff1b445f3740e6bf9d416ea500f07a9804
maulikvaghela <maulik.vagh@gmail.com>
2022-02-23 19:46:29 -0500

    updated

--- app.py
+++ app.py
@@ -35,15 +35,8 @@ def details(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('details.html', user = user)
 
-#star of edit....................
-
-@app.route("/<int:user_id>/edit")
-def edit(user_id):
-    user = User.query.get_or_404(user_id)
-    return render_template('/edit.html', user = user)
-   
-@app.route("/<int:user_id>/edit", methods = ["POST"])
-def aplly_edit(user_id):
+@app.route("/<int:user_id>", methods=['POST'])
+def detailed(user_id):
     user = User.query.get_or_404(user_id)
     first_name = request.form["new_first_name"]
     last_name = request.form["new_last_name"]
@@ -52,7 +45,19 @@ def aplly_edit(user_id):
     user = User(first = first_name, last = last_name, image = image)
     db.session.add(user)
     db.session.commit() 
-    return render_template (f"/{user.id}", user = user)
+    return render_template('details.html', user = user)
+#star of edit....................
+
+@app.route("/<int:user_id>/edit")
+def edit(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('/edit.html', user = user)
+   
+@app.route("/<int:user_id>/edit", methods = ["POST"])
+def apply_edit(user_id):
+    user = User.query.get_or_404(user_id)
+    
+    return render_template ("details.html", user = user)
 #end of editing....................
 
 #start of deleting.................
